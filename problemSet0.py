89e6a3fa6cd05a961947693565bfa3b12442cbb9
Tyler Furby <tyler@tylerfurby.com>
2017-08-13 17:32:59 -0600

    Initial commit

---
+++ problemSet0.py
@@ -0,0 +1,8 @@
+# MIT OpenCourseWare
+# Problem Set 0
+# Name: Tyler Furby
+
+def firstLastName():
+    lastName = raw_input("Enter your last name: ")
+    firstName = raw_input("Enter your first name: ")
+    return (lastName + "\n" + firstName)
