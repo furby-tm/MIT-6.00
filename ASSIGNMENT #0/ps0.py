51f146986f3f40c6e8c5d1208b1c2890fe50943f
Tyler Furby <tyler@tylerfurby.com>
2017-08-13 17:50:54 -0600

    -bash

---
+++ ASSIGNMENT #0/ps0.py
@@ -0,0 +1,9 @@
+# MIT 6.00 - Introduction to Computer Science and Programming
+# Problem Set 0
+# https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-00-introduction-to-computer-science-and-programming-fall-2008/assignments/pset0.pdf
+# Name: Tyler Furby
+
+def firstLastName():
+    lastName = raw_input("Enter your last name: ")
+    firstName = raw_input("Enter your first name: ")
+    return (lastName + "\n" + firstName)
